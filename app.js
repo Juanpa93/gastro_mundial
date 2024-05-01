1d49d6da297ae6005e0e39e81634831e164c4f29
Juanpa93 <66753879+Juanpa93@users.noreply.github.com>
2024-05-01 17:18:26 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,324 @@
+var filtro = '';
+var vConsulta = '';
+var filtro2 = '';
+var vConsulta2 = '';
+var url = '';
+var categorias = '';
+var areas = '';
+var ingredientes = '';
+var valorTemp = "";
+var criterioTemp = "";
+var banBA = "";
+
+function did(id) {
+  return document.getElementById(id)
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+listarValores('p_categorias', 'https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+listarValores('p_ingredientes', 'https://www.themealdb.com/api/json/v1/1/list.php?i=list');
+listarValores('p_areas', 'https://www.themealdb.com/api/json/v1/1/list.php?a=list');
+
+var urlParams = new URLSearchParams(window.location.search);
+
+vConsulta2 = urlParams.get('consulta');
+filtro2 = urlParams.get('filtro');
+banBA = urlParams.get('ba')
+
+if (vConsulta2 != null && filtro2 != null) {
+  document.getElementById('valor_consulta').value = vConsulta2
+  document.getElementById('filtro').value = filtro2
+  filtro = filtro2;
+  if(banBA == 'si') did('ck_ba').checked = true;
+  setTimeout(function(){
+    document.getElementById('ctr_btns_3').click();
+  },1000)
+}
+
+  did('ck_ba').addEventListener('input', function () {
+    console.log(this.checked);
+    if (this.checked) {
+      //
+      did('valor_consulta').value = '';
+      did('container_platos_galeria').innerHTML = '';
+      did('ctr_btns_3').style.display = 'none';
+      did('filtro').value = "Nombre";
+      did('filtro').setAttribute("readonly", true)
+          filtro = "Nombre";
+      did('valor_consulta').addEventListener('keyup', function(){
+        if(did('valor_consulta').value != ''){
+          did('ctr_btns_3').click()
+        }
+      })
+    } else {
+      did('ctr_btns_3').style.display = 'block';
+      did('filtro').value = "";
+          filtro = "";
+          did('filtro').removeAttribute("readonly")
+          did('valor_consulta').removeEventListener('keyup', function(){
+            if(did('valor_consulta').value != ''){
+              did('ctr_btns_3').click()
+            }
+          })
+    }
+  })
+
+  did('ctr_btns_1').addEventListener('click', function () {
+    did('valor_consulta').value = "";
+    did('filtro').value = "";
+    did('container_platos_galeria').innerHTML = "";
+    filtro = "";
+  })
+
+  did('ctr_btns_3').addEventListener('click', function () {
+
+    vConsulta = document.getElementById('valor_consulta').value
+    console.log(vConsulta, "- ", filtro)
+
+    if (vConsulta != '') {
+      console.log('vConsulta: ', vConsulta)
+    } else {
+      alert('Ingrese un valor a consultar')
+      return false;
+    }
+    
+    if((filtro == 'Área' && !areas.includes(vConsulta))){
+      alert("Ingrese un valor para Área permitido")
+      vConsulta = valorTemp
+      filtro = criterioTemp
+      did('valor_consulta').value = valorTemp;
+      did('filtro').value = criterioTemp; 
+      return false;
+    }
+
+    if((filtro == 'Ingrediente principal' && !ingredientes.includes(vConsulta))){
+      alert("Ingrese un valor para Ingrediente permitido")
+      vConsulta = valorTemp
+      filtro = criterioTemp
+      did('valor_consulta').value = valorTemp;
+      did('filtro').value = criterioTemp;
+      return false;
+    }
+
+    if((filtro == 'Categoría' && !categorias.includes(vConsulta))){
+      alert("Ingrese un valor para Categoría permitido")
+      vConsulta = valorTemp
+      filtro = criterioTemp
+      did('valor_consulta').value = valorTemp;
+      did('filtro').value = criterioTemp;
+      return false;
+    }
+
+    if (filtro != '' && ['Nombre', 'Área', 'Ingrediente principal', 'Categoría'].includes(filtro)) {
+      console.log('filtro: ', filtro)
+
+      switch (filtro) {
+
+        case 'Nombre':
+          url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${vConsulta}`;
+          fetch(url)
+            .then(function (response) {
+
+              if (!response.ok) {
+                throw new Error('La respuesta de la red no fue exitosa');
+              }
+              return response.json();
+            })
+            .then(function (data) {
+              console.log('Datos recibidos:', data);
+
+              if (data.meals != null) {
+                if (data.meals.length == 1) {
+                  var urlProductos = 'http://127.0.0.1:5500/prac4/productos.html?nombre=' + encodeURIComponent(vConsulta);
+                  + '&filtro=' + encodeURIComponent(filtro)
+                  window.location.href = urlProductos
+
+                } else {
+                  renderizarProductos(data)
+                }
+                valorTemp = vConsulta
+                criterioTemp = filtro
+              } else {
+                alert("No se ha encontrado el plato con los valores proporcionados")
+              }
+            })
+            .catch(function (error) {
+              console.log("Error en la ejecución")
+            });
+          break;
+
+        case 'Ingrediente principal':
+          url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${vConsulta}`;
+          fetch(url)
+            .then(function (response) {
+
+              if (!response.ok) {
+                throw new Error('La respuesta de la red no fue exitosa');
+              }
+              return response.json();
+            })
+            .then(function (data) {
+              console.log('Datos recibidos:', data);
+              if (data.meals != null) {
+                renderizarProductos(data);
+              } else {
+                alert("No se ha encontrado el plato con los valores proporcionados")
+              }
+            })
+            .catch(function (error) {
+              console.log("Error consultando al API")
+            });
+          break;
+        case 'Categoría':
+          url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${vConsulta}`;
+          fetch(url)
+            .then(function (response) {
+
+              if (!response.ok) {
+                throw new Error('La respuesta de la red no fue exitosa');
+              }
+              return response.json();
+            })
+            .then(function (data) {
+              console.log('Datos recibidos:', data);
+              if (data.meals != null) {
+                renderizarProductos(data);
+              } else {
+                alert("No se ha encontrado el plato con los valores proporcionados")
+              }
+            })
+            .catch(function (error) {
+              console.log("Error consultando al API")
+            });
+          break;
+
+        case 'Área':
+          url = `https://www.themealdb.com/api/json/v1/1/filter.php?a=${vConsulta}`;
+          fetch(url)
+            .then(function (response) {
+
+              if (!response.ok) {
+                throw new Error('La respuesta de la red no fue exitosa');
+              }
+              return response.json();
+            })
+            .then(function (data) {
+              console.log('Datos recibidos:', data);
+              if (data.meals != null) {
+                renderizarProductos(data);
+              } else {
+                alert("No se ha encontrado el plato con los valores proporcionados")
+              }
+            })
+            .catch(function (error) {
+              console.log("Error consultando al API")
+            });
+          break;
+      }
+    } else {
+      alert('seleccione un filtro válido')
+      return false;
+    }
+
+  })
+
+ 
+
+  console.log("retorno: ", vConsulta2, " - ", filtro2)
+
+  var inputElement = document.getElementById("filtro");
+
+
+  inputElement.addEventListener("input", function (event) {
+    filtro = inputElement.value;
+    console.log(filtro);
+  });
+  setTimeout(function(){
+    console.log('Categorias: ', categorias)
+    console.log('Areas: ', areas)
+    console.log('Ingredientes: ', ingredientes)
+  },2500)
+
+})
+
+function renderizarProductos(data) {
+  var contenedor = document.getElementById("container_platos_galeria");
+  contenedor.innerHTML = ''
+  var name = '';
+  console.log("rec en fn: ", data)
+  var iteraciones = (data.meals.length <= 18) ? data.meals.length - 1 : 17;
+  for (var i = 0; i <= iteraciones; i++) {
+    var nuevoDiv = document.createElement("button");
+
+
+    nuevoDiv.classList.add('platos')
+    var vIdentificador = data.meals[i].strMeal.replaceAll(' ', '-')
+    name = data.meals[i].strMeal;
+    nuevoDiv.id = `card_${vIdentificador}`
+    nuevoDiv.innerHTML = `
+  <h5 id="h2_${vIdentificador}">${data.meals[i].strMeal}</h5>
+  <img id="img_${vIdentificador}" style="height: 100px;" src="${data.meals[i].strMealThumb}" alt="">`;
+    console.log(nuevoDiv)
+
+
+
+    nuevoDiv.addEventListener('click', function (event) {
+      var auxBA = did('ck_ba').checked ? 'si' : 'no' 
+      console.log(event.target.id)
+      var vValorConsulta = event.target.id.replaceAll('-', ' ').replace('card_', '').replace('h2_', '').replace('img_', '')
+      var urlProductos2 = 'http://127.0.0.1:5500/prac4/productos.html?nombre=' + encodeURIComponent(vValorConsulta) +
+        '&consulta=' + encodeURIComponent(vConsulta) + '&filtro=' + encodeURIComponent(filtro) + '&ba=' + encodeURIComponent(auxBA)
+      console.log(i, ": ", urlProductos2)
+      window.location.href = urlProductos2;
+     
+    })
+    contenedor.appendChild(nuevoDiv);
+    valorTemp = vConsulta
+    criterioTemp = filtro
+    console.log("redner: ", valorTemp, criterioTemp)
+  }
+
+}
+
+function listarValores(elemento, url){
+  var valores = '';
+  fetch(url)
+
+  .then(function(response) {
+
+    if (!response.ok) {
+      throw new Error('La respuesta de la red no fue exitosa');
+    }
+    return response.json();
+  })
+  .then(function(data) {
+    console.log('lista recibida:', data);
+    switch(elemento){
+      case 'p_categorias':
+        data.meals.forEach(element => {
+          valores += element.strCategory +', '
+        });
+        categorias = valores.split(', ')
+        break;
+        case 'p_ingredientes':
+        data.meals.forEach(element => {
+          valores += element.strIngredient +', '
+        });
+        ingredientes = valores.split(', ')
+        break;
+        case 'p_areas':
+        data.meals.forEach(element => {
+          valores += element.strArea +', '
+        });
+        areas = valores.split(', ')
+        break;
+
+    }
+    
+    did(elemento).innerText = valores.slice(0,-2).split(',').sort().join(', ') + '.'
+  })
+  .catch(function(error) {
+    console.log("Error en la ejecución")
+  });
+}
+
---
+++ app_productos.js
@@ -0,0 +1,87 @@
+var filtro = '';
+var vConsulta = '';
+var url = '';
+var auxBA = '';
+
+//www.themealdb.com/api/json/v1/1/list.php?c=list
+function dqs(qs) {
+  return document.querySelector(qs);
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+
+  var urlParams = new URLSearchParams(window.location.search);
+  var nombre = urlParams.get('nombre');
+  vConsulta = urlParams.get('consulta');
+  auxBA = urlParams.get('ba');
+  filtro = urlParams.get('filtro');
+    console.log("Nombre recibido: ", nombre)
+
+  url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${nombre}`;
+  fetch(url)
+
+  .then(function(response) {
+
+    if (!response.ok) {
+      throw new Error('La respuesta de la red no fue exitosa');
+    }
+    return response.json();
+  })
+  .then(function(data) {
+    console.log('Datos recibidos:', data);
+
+    if(data.meals != null){
+        var contenedor = document.getElementById("container_platos");
+
+        var nuevoDiv = document.createElement("div");
+
+        var ingredientesConcatenados = "";
+
+
+for (var i = 1; i <= 20; i++) {
+  var atributo = "strIngredient" + i;
+  if (data.meals[0].hasOwnProperty(atributo)) {
+    if(data.meals[0][atributo] != null && data.meals[0][atributo] != '') ingredientesConcatenados += ' ✔ ' + data.meals[0][atributo] + " <br> ";
+  } 
+}
+
+ingredientesConcatenados = ingredientesConcatenados.slice(0, -2);
+
+console.log("Ingredientes concatenados:", ingredientesConcatenados);
+
+    nuevoDiv.classList.add('platos')
+        nuevoDiv.innerHTML = `
+        <h2 id='titulo_plato_v2'>${data.meals[0].strMeal}</h2>
+        <img id='img_plato_v2' style="height: 150px;" src="${data.meals[0].strMealThumb}" alt="">
+            <div id='div1_plato_v2'><h3>Ingredientes: </h3> <p>${ingredientesConcatenados}</p></div>
+            <div id='div2_plato_v2'><h3>Instrucciones: </h3> <p>${data.meals[0].strInstructions}</p></div>
+            <div id='div3_plato_v2'><h3>Etiquetas: </h3> <p>${(data.meals[0].strTags == null) ?'No disponibles': data.meals[0].strTags}</p></div>`;
+    console.log(nuevoDiv)
+    
+    contenedor.appendChild(nuevoDiv);
+    dqs('#div1_plato_v2').style.display = 'none'
+    dqs('#div2_plato_v2').style.display = 'none'
+    dqs('#div3_plato_v2').style.display = 'none'
+    }else{
+        alert("No se ha encontrado el plato con los valores proporcionados")
+    }
+  })
+  .catch(function(error) {
+    console.log("Error en la ejecución")
+  });
+  
+ 
+  
+  
+  
+
+    setTimeout(function(){dqs('#div1_plato_v2').style.display = 'block'},400)
+    setTimeout(function(){dqs('#div2_plato_v2').style.display = 'block'},500)
+    setTimeout(function(){dqs('#div3_plato_v2').style.display = 'block'},600)
+  document.getElementById('btn_home').addEventListener('click', function(){
+    window.location.href = 'http://127.0.0.1:5500/prac4/index.html?' +
+    'consulta=' + encodeURIComponent(vConsulta) +  '&filtro=' + encodeURIComponent(filtro)
+     + '&ba=' + encodeURIComponent(auxBA);
+  });
+  });
+
